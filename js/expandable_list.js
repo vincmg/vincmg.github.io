3793b88589601104baf68ded3700355b15d47ac4
VinCMG <vincentchang9999@gmail.com>
2018-06-22 22:38:59 -0700

    add width 80% on music list but it doesn't work for large screen widths

---
+++ js/expandable_list.js
@@ -0,0 +1,15 @@
+function toggle_list(id) {
+  let element = document.getElementById(id);
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+// set table display to none here instead of CSS because ???
+document.getElementById("releases").style.display = 'none';
+document.getElementById("compilations").style.display = 'none';
+
+document.getElementById("releases-toggle").onclick = () => { toggle_list("releases") };
+document.getElementById("compos-toggle").onclick = () => { toggle_list("compilations") };
