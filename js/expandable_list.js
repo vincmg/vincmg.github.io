da54f9bce55bc79dd5c148970fe1321af73e127d
sherwyn <sherwyn.sen@gmail.com>
2018-06-23 00:14:11 -0700

    ddddddd

--- js/expandable_list.js
+++ js/expandable_list.js
@@ -1,6 +1,6 @@
 function toggle_list(id) {
   let element = document.getElementById(id);
-  if (element.style.display === "none") {
+  if (!element.style.display || element.style.display === "none") {
     element.style.display = "block";
   } else {
     element.style.display = "none";
@@ -8,8 +8,8 @@ function toggle_list(id) {
 }
 
 // set table display to none here instead of CSS because ???
-document.getElementById("releases").style.display = 'none';
-document.getElementById("compilations").style.display = 'none';
+//document.getElementById("releases").style.display = 'none';
+//document.getElementById("compilations").style.display = 'none';
 
 document.getElementById("releases-toggle").onclick = () => { toggle_list("releases") };
 document.getElementById("compos-toggle").onclick = () => { toggle_list("compilations") };
