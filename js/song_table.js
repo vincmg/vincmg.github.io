bb0ee2afb9f037008aa0ae20e7d4ec4cc676445b
vincmg <vincmg>
2018-10-06 13:21:34 -0500

    fix song table

--- js/song_table.js
+++ js/song_table.js
@@ -3,10 +3,10 @@ let song_list;
 (function load_table() {
 
   let song_table_req = new XMLHttpRequest();
-  song_table_req.onreadystatechange = () => {
-    if (this.readyState === 4 && this.status === 200) {
+  song_table_req.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
       song_list = JSON.parse(this.responseText);
-      make_album_table(false); // alphabetical order
+      make_song_table(false); // alphabetical order
     }
   }
   song_table_req.open('GET', 'js/song_list.json', true);
@@ -14,7 +14,7 @@ let song_list;
 })();
 
 // have to copy paste this from album_table.js because ES6 module imports are not supported???
-function make_album_table(reverse) {
+function make_song_table(reverse) {
   let table_list = reverse ? song_list.reverse() : song_list;
   let table = document.getElementById("misc-songs");
   table.innerHTML += `
@@ -27,11 +27,15 @@ function make_album_table(reverse) {
   for (let i = 0; i < table_list.length; i++) {
     let song = table_list[i];
     let formatted_info = song.info.replace(/\n/g, '<br>')
+    let link_name = "download";
+    if (song.link === "TODO") {
+      link_name = "not available yet";
+    }
     table.innerHTML += `
     <tr>
       <td>${song.name}</td>
       <td>${formatted_info}</td>
-      <td><a href="${song.link}">download coming soon</a></td>
+      <td><a href="${song.link}" download>${link_name}</a></td>
     </tr>`;
   }
 }
