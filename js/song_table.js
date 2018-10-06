470cfc36dd6ae3f82acc2e1eed190982800dda10
VinCMG <vincentchang9999@gmail.com>
2018-10-06 13:08:05 -0500

    test

--- js/song_table.js
+++ js/song_table.js
@@ -1,15 +1,17 @@
-
 let song_list;
 
-let song_table_req = new XMLHttpRequest();
-song_table_req.onreadystatechange = () => {
-  if (this.readyState === 4 && this.status === 200) {
-    song_list = JSON.parse(this.responseText);
-    make_album_table(false); // alphabetical order
+(function load_table() {
+
+  let song_table_req = new XMLHttpRequest();
+  song_table_req.onreadystatechange = () => {
+    if (this.readyState === 4 && this.status === 200) {
+      song_list = JSON.parse(this.responseText);
+      make_album_table(false); // alphabetical order
+    }
   }
-}
-song_table_req.open('GET', 'js/song_list.json', true);
-song_table_req.send();
+  song_table_req.open('GET', 'js/song_list.json', true);
+  song_table_req.send();
+})();
 
 // have to copy paste this from album_table.js because ES6 module imports are not supported???
 function make_album_table(reverse) {
