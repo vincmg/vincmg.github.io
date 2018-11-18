4d7dd318f25c355b4dfa1d32ed2a2eb10ece01c0
VinCMG <vincentchang9999@gmail.com>
2018-11-18 00:46:52 -0600

    tweak text, footer

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,6 @@
 (function make_footer() {
   let footer_div = document.getElementById("footer");
-  footer_div.innerHTML = "last updated " + document.lastModified.substring(0, 10);
+  footer_div.innerHTML = `all songs are <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons BY-NC-SA 4.0</a> unless otherwise noted<br>`
+    + "last updated " + document.lastModified.substring(0, 10);
+
 })();
