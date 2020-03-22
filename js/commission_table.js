c961dc6a28ee3d7bc9459af430b46773ecc72b13
VinCMG <vincentchang9999@gmail.com>
2020-03-21 21:51:05 -0500

    add bandcamp embeds for commissions

--- js/commission_table.js
+++ js/commission_table.js
@@ -2,14 +2,22 @@ const commission_list = [
     {
         "name": "Breaking News! Intro",
         "info": "written for a podcast series by <a href=\"https://octothorpic.com/\">octothorpic</a>",
-        "link": "https://octothorpic-music.bandcamp.com/album/breaking-news"
+        "art": "img/octothorpe.jpg",
+        "embed": `<iframe style="border: 0; width: 200px; height: 200px;" src="https://bandcamp.com/EmbeddedPlayer/album=2293678488/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://octothorpic-music.bandcamp.com/album/breaking-news">Breaking News! by Octothorpic</a></iframe>`
     },
     {
       "name": "Gem Blenders Soundtrack",
       "info": "written for the <a href=\"https://www.kickstarter.com/projects/stevesekula/gem-blenders-the-card-game\">Gem Blenders Kickstarter Campaign</a>",
-      "link": "https://vincekaichan.bandcamp.com/album/gem-blenders-soundtrack"
+      "art": "img/gem_blenders.jpg",
+      "embed": `<iframe style="border: 0; width: 200px; height: 200px;" src="https://bandcamp.com/EmbeddedPlayer/album=1560225786/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://vincekaichan.bandcamp.com/album/gem-blenders-soundtrack">Gem Blenders Soundtrack by Vince Kaichan</a></iframe>`
     }
 ];
+
+// have to copy paste this from album_table.js because ES6 module imports are not supported???
+function load_bandcamp_embed(element, commission_embed) {
+  let td_parent = element.parentElement;
+  td_parent.innerHTML = commission_embed;
+}
 
 // have to copy paste this from album_table.js because ES6 module imports are not supported???
 (function make_commission_table(reverse) {
@@ -19,26 +27,25 @@ const commission_list = [
   <tr> 
     <th>name</th>
     <th>info</th>
-    <th>link</th>
+    <th>art <br> (click to play)</th>
   </tr>`;
 
-  for (let commission of table_list) {
+  for (let [index, commission] of table_list.entries()) {
     let formatted_info = commission.info.replace(/\n/g, '<br>')
-    let link_name = "download";
-    if (commission.link === "TODO") {
-      link_name = "no download yet";
-    } else if (commission.link.search(/soundcloud.com/g) != -1) {
-      link_name = "soundcloud";
-    }
-    else {
-      // TODO TODO put bandcamp embed here
-    }
     table.innerHTML += `
     <tr>
       <td>${commission.name}</td>
       <td>${formatted_info}</td>
-      <td><a href="${commission.link}" download>${link_name}</a></td>
+      <td>
+        <img class="album-art" id="commission_${index}" src="${commision.art}" alt="${commision.name}">
+      </td
     </tr>`;
+  }
+  for (let [index, commission] of table_list.entries()) {
+    let img = document.getElementById("commission_" + index);
+    img.addEventListener("click", () => {
+      load_bandcamp_embed(img, commission.embed);
+    });
   }
 })();
 
