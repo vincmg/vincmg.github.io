209635383696025f29935c05dd916fcad36182e3
VinCMG <vincentchang9999@gmail.com>
2021-01-13 15:39:57 -0600

    initial commit with jinja

---
+++ generate.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# TODO TODO
+# load jinja template
+# load json files
+# figure out how to attach the event listeners in the template
+# compile template
+
+if __name__ == '__main__':
+    print("TODO TODO")
+
