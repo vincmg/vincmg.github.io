9cdbf04b6fe8be9cde8fa25f4a7bff7878b5fcf8
VinCMG <vincentchang9999@gmail.com>
2021-03-08 10:27:30 -0600

    clear node before repopulating it

--- js/table_sort_by_column.js
+++ js/table_sort_by_column.js
@@ -12,12 +12,11 @@ function sort_by_column(table, column_name) {
   // TODO TODO add a span around the non-header rows so we don't have to add the header row this way afterwards
   content.splice(0, 0, rows[0]);
 
-  // TODO TODO this is way too slow
   let collection = document.createDocumentFragment();
   for (e of content) {
     collection.appendChild(e);
   }
-  console.log(collection);
+  table.children[0].innerHTML = '';
   table.children[0].appendChild(collection);
 }
 
