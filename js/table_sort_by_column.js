bf70f031e81642162bc70e3142f1b5fc38dad480
VinCMG <vincentchang9999@gmail.com>
2021-03-08 11:18:09 -0600

    reverse sort order if column is clicked on again

--- js/table_sort_by_column.js
+++ js/table_sort_by_column.js
@@ -1,15 +1,43 @@
-function sort_func(a, b, field) {
+function sort_func(a, b, field, ordering) {
   let a_content = a.querySelector(`[${field}]`).innerHTML;
   let b_content = b.querySelector(`[${field}]`).innerHTML;
-  return a_content.localeCompare(b_content);
+
+  if (ordering === "default" || ordering === "ascending") {
+    return a_content.localeCompare(b_content);
+  } else if (ordering === "descending") {
+    return -1 * a_content.localeCompare(b_content);
+  }
+
+  // else
+  console.error(`field ${field} had invalid sortorder state ${ordering}`);
 }
 
 function sort_by_column(table, column_name) {
   // table -> tbody -> rows
   let rows = Array.from(table.children[0].children);
+  let header = rows[0];
   let content = rows.slice(1);
-  content.sort((a, b) => { return sort_func(a, b, column_name); });
-  // TODO TODO add a span around the non-header rows so we don't have to add the header row this way afterwards
+
+  let column = header.querySelector(`[${column_name}]`);
+  let column_state = column.getAttribute("sortordering");
+
+  let sort_states = header.querySelectorAll("[sortordering]");
+  for (state of sort_states) {
+    state.setAttribute("sortordering", "default");
+  }
+
+  // default --> ascending
+  // ascending --> descending
+  // descending --> ascending
+  if (column_state === "default") {
+    column.setAttribute("sortordering", "ascending");
+  } else if (column_state === "ascending") {
+    column.setAttribute("sortordering", "descending");
+  } else if (column_state === "descending") {
+    column.setAttribute("sortordering", "ascending");
+  }
+
+  content.sort((a, b) => { return sort_func(a, b, column_name, column.getAttribute("sortordering")); });
   content.splice(0, 0, rows[0]);
 
   let collection = document.createDocumentFragment();
